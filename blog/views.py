5ff1566eb51107fd4641d06cc07f35125f4eec4b
Tony <zayniddinovibrat@gmail.com>
2022-02-10 09:58:41 +0500

    changes for tags, moving to previous/next blogs done!

--- blog/views.py
+++ blog/views.py
@@ -5,12 +5,20 @@ from .models import BlogPostModel
 
 class BlogListView(ListView):
     model = BlogPostModel
+    # queryset = BlogPostModel.objects.order_by('-pk')
     template_name = 'main/blog_list.html'
     context_object_name = 'posts'
+
+    def get_queryset(self):
+        qs = BlogPostModel.objects.order_by('-pk')
+        tag = self.request.GET.get('tag')
+        if tag:
+            return qs.filter(tags__title=tag)
+        else:
+            return qs
 
 
 class BlogDetailView(DetailView):
     model = BlogPostModel
     template_name = 'main/blog_detail.html'
     context_object_name = 'posts'
-
