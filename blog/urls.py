dd0924d27ef9f59cce9373b8db2af1593cd2835b
Tony <zayniddinovibrat@gmail.com>
2022-02-12 13:32:31 +0500

    comment part done!

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView
+from .views import BlogListView, BlogDetailView, BlogCommentView
 
 app_name = 'blogs'
 
 urlpatterns = [
+    path('<int:pk>/comments/', BlogCommentView.as_view(), name='blog_comments'),
     path('', BlogListView.as_view(), name='blog_list'),
     path('<int:pk>/post-details/', BlogDetailView.as_view(), name='blog_detail')
 ]
+
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from .models import BlogPostModel
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views.generic import ListView, DetailView, CreateView
 
+from .forms import CommentModelForm
+from .models import BlogPostModel, CommentModel
+
+
+#
 
 class BlogListView(ListView):
     model = BlogPostModel
@@ -22,3 +27,17 @@ class BlogDetailView(DetailView):
     model = BlogPostModel
     template_name = 'main/blog_detail.html'
     context_object_name = 'posts'
+
+#
+
+
+class BlogCommentView(CreateView):
+    model = CommentModel
+    form_class = CommentModelForm
+
+    def form_valid(self, form):
+        form.instance.post = get_object_or_404(BlogPostModel, id=self.kwargs.get('pk'))
+        return super(BlogCommentView, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse('blogs:blog_detail', kwargs={'pk': self.kwargs.get('pk')})
