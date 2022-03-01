b58e97c7eab9462cc6cb4e114f511edadaea48e3
Tony <zayniddinovibrat@gmail.com>
2022-03-01 22:58:45 +0500

    final changes of wishlist

--- product/views.py
+++ product/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import WishlistModel
 from django.contrib.auth.decorators import login_required
 from shop.models import ProductModel
+from django.views.generic import ListView
+
+
+class WishlistView(ListView):
+    template_name = 'main/wishlist.html'
+
+    def get_queryset(self):
+        return ProductModel.objects.all().filter(wishlist__user_id=self.request.user)
 
 
 @login_required
@@ -9,5 +17,4 @@ def update_wishlist(request, pk):
     product = get_object_or_404(ProductModel.objects.all().filter(id=pk))
     wishlist = WishlistModel.add_to_delete(request.user, product)
 
-    return redirect('shop:shop')
-
+    return redirect(request.GET.get('next', '/'))
